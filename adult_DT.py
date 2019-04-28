38e362af379a34d548ac81e017100a1b979444dc
Dmytro Kolesnykov <33485777+dmtrkl@users.noreply.github.com>
2019-04-29 02:10:02 +0300

    Update adult_DT.py

--- adult_DT.py
+++ adult_DT.py
@@ -34,7 +34,13 @@ for i in range(5):
     data = pd.read_csv("https://storage.googleapis.com/testbucket-week10/adult.data") #From Bucket in .... Region
     timeE2 = time.time()
     
+    
+    timeS3 = time.time()
+    data = pd.read_csv("https://storage.googleapis.com/dmytro-kolesnykov-week10/adult.data") #From Bucket in .... Region
+    timeE3 = time.time()
+    
     timeReg1 += timeE1 - timeS1
     timeReg2 += timeE2 - timeS2
+    timeReg3 += timeE3 - timeS3
     
-print("Time1:", timeReg1 / 5, " --- Time2:", timeReg2 / 5 )
+print("Time1:", timeReg1 / 5, " --- Time2:", timeReg2 / 5 , " --- Time3:", timeReg3 / 5 )
